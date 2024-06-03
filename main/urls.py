b32c614d83ee7262d98940c316f79471fcb6cb79
DmytroSerhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-06-03 23:02:03 +0300

    Update push.yml

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ app_name = "main"
 urlpatterns = [
     path('', views.index, name='home'),
     re_path(r'^shop/?$', views.shop, name='shop'),
-    re_path(r'^shop/(?P<category_link>\w+)/?$', views.category, name='category'),
+    re_path(r'^shop/(?P<category_link>.+)/?$', views.category, name='category'),
 ]
