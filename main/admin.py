2dfffe879223088879c6ad520c25eeb7120a7095
DmytroSerhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-06-04 16:55:53 +0300

    fix

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, Product, Brand
+
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Brand)
-admin.site.register(Order)
-admin.site.register(OrderItem)
