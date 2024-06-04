1c084293f340c9528695256d981164ef9cb64bc5
DmytroSerhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-06-04 17:06:38 +0300

    Added test for user app

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from user.models import User
 
 # Create your tests here.
+
+class AuthTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(email='test@test.com', password='111')
+
+    def test_auth_requirements(self):
+        self.client.logout()
+        response = self.client.get(reverse('user:profile'))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.login(email='test@test.com', password='111')
+        response = self.client.get(reverse('user:profile'))
+        self.assertEqual(response.status_code, 200)
